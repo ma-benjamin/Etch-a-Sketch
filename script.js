c49b0064048ae1a34697974f9307619c3c6be46b
Benjamin Ma <ben1002.ma@gmail.com>
2022-04-28 17:26:53 -0400

    Added size slider

--- script.js
+++ script.js
@@ -4,12 +4,26 @@ let color = '#333333'
 
 const grid = document.getElementById('grid')
 const clearBtn = document.getElementById('clearBtn')
+const sizeSlider = document.getElementById('sizeSlider')
+const sizeDisplay = document.getElementById('sizeDisplay')
 
 clearBtn.onclick = () => reload()
+sizeSlider.onmousemove = (e) => updateSizeDisplay(e.target.value)
+sizeSlider.onchange = (e) => changeBoardSize(e.target.value)
 
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
+
+function updateSizeDisplay(s) {
+    sizeDisplay.innerHTML = `${s} x ${s}`
+}
+
+function changeBoardSize(s) {
+    size = s
+    updateSizeDisplay(s)
+    reload()
+}
 
 function reload() {
     clear()
